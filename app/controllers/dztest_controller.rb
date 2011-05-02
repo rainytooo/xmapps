730e9b9d3e1a9b938fc68adcdd0b0feb48198f4f
wang chao <wangchao@xiaoma.com>
2011-05-02 19:41:52 +0800

    change gitignore

--- app/controllers/dztest_controller.rb
+++ app/controllers/dztest_controller.rb
@@ -5,6 +5,8 @@ class DztestController < ApplicationController
   def index
 	passwd_uid = decode_cookie(Rails.configuration.dz_auth_key, request.user_agent, cookies, Rails.configuration.dz_cookie_name)
 	logger.info passwd_uid[0] + 'and' + passwd_uid[1]
+	#@teststr = authcode(dz_cookie_value,true, key_code)
+	#@dzuser = Dzuser.find(1)
 	respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @campaigns }
