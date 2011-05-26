a3a98bffe7bebd48af2bc30b8da88769a73c28ba
wang chao <wangchao@xiaoma.com>
2011-05-27 02:25:54 +0800

    css modiy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
   def index
-	require 'iconv'
-	origin_filename = '按时打算'
-	request.env['HTTP_USER_AGENT'].match('MSIE')  ? Iconv.conv("utf8", "gb2312", origin_filename) : origin_filename
-	logger.info origin_filename
   end
   # 页面无法找到
   def notfound
