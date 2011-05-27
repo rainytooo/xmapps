31b88849dc978ba794b8bce417da6dc43dc1cec9
wang chao <wangchao@xiaoma.com>
2011-05-27 15:50:28 +0800

    update dl type views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110526112954) do
+ActiveRecord::Schema.define(:version => 20110524031348) do
 
   create_table "applies", :force => true do |t|
     t.string   "name"
@@ -109,7 +109,6 @@ ActiveRecord::Schema.define(:version => 20110526112954) do
     t.string   "photo_file_name"
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
-    t.datetime "photo_updated_at"
   end
 
   add_index "dl_threads", ["dl_type_id"], :name => "fk_dlthreads_dltype"
