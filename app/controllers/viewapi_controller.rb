63a4d2c4ccd1a9057e6fad672d63558a8d107b45
wangchao <ohergal@gmail.com>
2011-06-10 19:32:04 +0800

    update asks api

--- app/controllers/viewapi_controller.rb
+++ app/controllers/viewapi_controller.rb
@@ -2,7 +2,7 @@ class ViewapiController < ApplicationController
   # 所有新问题
   def asks_new
     # 查询最新的问题
-    @new_asks = Ask.find_by_sql("SELECT  * FROM asks ORDER by created_at desc limit 10 OFFSET 0 ")
+    @new_asks = Ask.find_by_sql("SELECT  * FROM asks ORDER by created_at desc limit 9 OFFSET 0 ")
     # 写出文本
     js_text = "document.write('"
     @new_asks.each do |ask|
@@ -17,15 +17,15 @@ class ViewapiController < ApplicationController
   # 所有新解决的问题
   def asks_closed
     # 查询最新解决的问题
-    @closed_asks = Ask.find_by_sql("SELECT  * FROM asks where status = 1 or status = 2 ORDER by created_at desc limit 10 OFFSET 0 ")
+    @closed_asks = Ask.find_by_sql("SELECT  * FROM asks where status = 1 or status = 2 ORDER by created_at desc limit 9 OFFSET 0 ")
     # 写出文本
-    js_text = "document.write('<div class=\"module cl xl xl1\">\n<ul>"
+    js_text = "document.write('"
     @closed_asks.each do |ask|
       js_text += "<li>"
       js_text += "<a title=\"#{ask.title}\" href=\"#{XMAPP_MAIN_DOMAIN_URL}/asks/#{ask.id}\">#{ask.title}"
       js_text += "</a></li>"
     end
-    js_text += "</ul>\n</div>');"
+    js_text += "');"
     render :text => js_text
   end
 
