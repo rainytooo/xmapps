247b02d75542db023050b6643a3e8db43c2d62d8
wangchao <ohergal@gmail.com>
2011-06-11 13:15:44 +0800

    add ask search and update some pages

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,35 +7,20 @@ class SearchController < ApplicationController
 		logger.debug @search_string
 		@search_string.strip
 		if @search_string
-			# 去掉多余的空格
-			@search_string.squeeze!("\s")
-			@origin_words = params[:query].strip
-			@origin_words.squeeze("\s")
-			@search_string = '%'+@search_string+'%'
-			@dl_threads = DlThread.where(" ispass = 1 and  ( name like :search_string  or   ispass = 1 and content_desc like :search_string )",
-			    {:search_string => @search_string}).order("created_at DESC").paginate(:page=>params[:page]||1,:per_page=>10)
-			if @dl_threads.empty?
-				flash.now[:message] = "对不起,没有搜索到相关内容,您可以在右侧点击我要上传来添加这个资源,可以获取大量积分和金币"
-				render 'dl_search.html'
-				return
-			end
-			# 拿出最多上传的用户
-			#@rank_users = User.find_by_sql('select * from users LEFT OUTER JOIN user_counts ON user_counts.user_id = users.id where user_counts.uploads != 0 order by user_counts.uploads desc limit 0, 12')
-			# 拿出最热下载
-			#@hot_dl_threads = DlThread.where("ispass = ?", 1).order("views DESC").limit(10).offset(0)
-			# 记录用户搜索的记录
-			search_keyword = SearchKeywords.new
-			if session[:login_user_id]
-				search_keyword.user_id = session[:login_user_id]
-			end
-			search_keyword.keywords = @origin_words
-			search_keyword.appname = 'dl'
-			search_keyword.save
-			render 'dl_search.html'
-			return
+		  # 去掉多余的空格
+		  @search_string.squeeze!("\s")
+		  @origin_words = params[:query].strip
+		  @origin_words.squeeze("\s")
+		  @search_string = '%'+@search_string+'%'
+		  if search_type == "dl"
+        dl_search
+      elsif search_type == "ask"
+        ask_search
+	    end
 		end
 
 	end
+
 	def sockettest
 	  logger.debug 'start a tcp socket ,and send a message'
 	  socket_client
@@ -43,8 +28,50 @@ class SearchController < ApplicationController
 
 	private
 
-		def dl_search
+	  def search_dl
 
+	  end
+	  # 记录用户的搜索
+	  def save_user_keyword(type_str)
+	    search_keyword = SearchKeywords.new
+		  if session[:login_user_id]
+			  search_keyword.user_id = session[:login_user_id]
+		  end
+		  search_keyword.keywords = @origin_words
+		  search_keyword.appname = type_str
+		  search_keyword.save
+   end
+
+	  # 问答的搜索
+	  def ask_search
+      @asks = Ask.where("title like :search_string or content like :search_string",
+          {:search_string => @search_string}).order("created_at DESC").paginate(:page=>params[:page]||1,:per_page=>10)
+      if @asks.empty?
+        flash.now[:message] = "对不起,没有搜索到相关内容,您可以在右侧点击我要提问来提出这个问题"
+			   render 'ask_search.html'
+			   return
+      end
+      save_user_keyword 'ask'
+		  render 'ask_search.html'
+		  return
+    end
+    # 简单的搜索下载
+		def dl_search
+		  @dl_threads = DlThread.where(" ispass = 1 and  ( name like :search_string  or   ispass = 1 and content_desc like :search_string )",
+		      {:search_string => @search_string}).order("created_at DESC").paginate(:page=>params[:page]||1,:per_page=>10)
+		  if @dl_threads.empty?
+			  flash.now[:message] = "对不起,没有搜索到相关内容,您可以在右侧点击我要上传来添加这个资源,可以获取大量积分和金币"
+			  render 'dl_search.html'
+			  return
+		  end
+		  # 拿出最多上传的用户
+		  #@rank_users = User.find_by_sql('select * from users LEFT OUTER JOIN user_counts ON user_counts.user_id = users.id where user_counts.uploads != 0 order by user_counts.uploads desc limit 0, 12')
+		  # 拿出最热下载
+		  #@hot_dl_threads = DlThread.where("ispass = ?", 1).order("views DESC").limit(10).offset(0)
+		  # 记录用户搜索的记录
+		  save_user_keyword 'dl'
+		  render 'dl_search.html'
+		  return
 		end
 
 		def socket_client
