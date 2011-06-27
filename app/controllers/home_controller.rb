063cf5673356fbd2eb76d23b58060589a13f5ea2
wangchao <ohergal@gmail.com>
2011-06-27 09:35:05 +0800

    update index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,15 @@
 class HomeController < ApplicationController
   def index
+    # 取出最新的下载
+    # 本周热门
+	  date_after = Date.today - 14
+	  @week_hot = DlThread.where("ispass = ? and created_at >= ?", 1, date_after).order("views DESC").order("created_at DESC").limit(10).offset(0)
+	  # 最新免金币下载资源
+    @free_dl = DlThread.where("ispass = 1").order("created_at DESC").limit(10).offset(0)
+    # 最新问答
+    @new_asks = Ask.order("created_at DESC").limit(10)
   end
-  
+
   # def robots
 	# robots = ROBOTS_TXT
     # respond_to do |format|
@@ -15,3 +23,4 @@ class HomeController < ApplicationController
   def errorpage
   end
 end
+
