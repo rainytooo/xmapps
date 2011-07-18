88411dc717a2a40c9acdd2082a18e927b21eaa8b
wangchao <ohergal@gmail.com>
2011-07-18 13:54:48 +0800

    track code

--- app/controllers/baomings_controller.rb
+++ app/controllers/baomings_controller.rb
@@ -1,15 +1,15 @@
 class BaomingsController < ApplicationController
   def create
-    baoming = Baoming.new
-    baoming.name = params[:name]
-    baoming.telnum = params[:telnum]
-    baoming.zhuanye = params[:zhuanye]
-    baoming.baokaoxuexiao = params[:baokaoxuexiao]
-    baoming.tuofucj = params[:tuofucj]
-    baoming.campaign = params[:campaign]
+    @baoming = Baoming.new
+    @baoming.name = params[:name]
+    @baoming.telnum = params[:telnum]
+    @baoming.zhuanye = params[:zhuanye]
+    @baoming.baokaoxuexiao = params[:baokaoxuexiao]
+    @baoming.tuofucj = params[:tuofucj]
+    @baoming.campaign = params[:campaign]
     bm_flag = params[:flag]
     if bm_flag.to_s == '1'
-      if baoming.save
+      if @baoming.save
         flash[:message] = "报名成功"
         redirect_to :action => "bm_result2"
         #flash.now[:message] = "报名成功"
@@ -21,7 +21,7 @@ class BaomingsController < ApplicationController
 		    #render '/errors_messages.html'
       end
     else
-      if baoming.save
+      if @baoming.save
         flash[:message] = "报名成功"
         redirect_to :action => "bm_result"
         #flash.now[:message] = "报名成功"
